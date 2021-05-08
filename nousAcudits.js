7326abbcfffee2b3f76d52ed2ad6460e6a141710
Nuria Gardenyes <gardenyesnuria@gmail.com>
2021-05-08 20:44:37 +0200

    nivell2 exercici3

--- nousAcudits.js
+++ nousAcudits.js
@@ -28,3 +28,15 @@ function apiAcuditsJson(){
         });
 }
 
+
+let temps;   
+fetch('https://api.weatherbit.io/v2.0/current?city=barcelona&lang=ca&key=ae17724ed86745d0a88224826999484a')
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(response){
+            //console.log(response)
+            console.log(response.data[0].weather.description)
+            temps = response.data[0].weather.description;
+            document.querySelector("#mostrarTemps").innerHTML = temps;
+        });
